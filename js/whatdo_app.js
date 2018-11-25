c0c57c06396c15744f2989aa7ac47fd07baba61a
Corey <cbourke98@hotmail.com>
2018-11-25 14:03:41 +1300

    Added Maps

--- js/whatdo_app.js
+++ js/whatdo_app.js
@@ -1,3 +1,5 @@
+//Maps api key
+//AIzaSyAq3zs8ekzWJL5GYx8z1nYEMTpjkMzPvhc
 var app = angular.module("Whatdo",[]);
 
 app.filter('reverse', function() {
@@ -13,12 +15,14 @@ app.controller('mController', function($scope, $interval, $http) {
     $scope._data = null;
     $scope.activity;
     $scope.last = '';
+    $scope.onlineStatus = true;
+    $scope.myLocation=null;
 
     $scope.newActivity = function(obj){
       this.name = obj.name;
       this.longLocation = obj.location_summary;
       this.startTime = obj.datetime_start
-
+      this.pointLocation = obj.point
       this.keyWords = function(){
         //Return keywords for user to dislike / dislike
         //avoid returning location_slug
@@ -39,6 +43,7 @@ app.controller('mController', function($scope, $interval, $http) {
         this.longLocation = '';
         this.startTime = '';
         this.allowLocation = _location;
+        this.point = '';
       }
       $scope.activity = new activityObj(activityName);
       $scope.last = $scope.activity.name;
@@ -48,28 +53,49 @@ app.controller('mController', function($scope, $interval, $http) {
       //fetch function / event activity from eventfinda api
       $('.loader').show();
       if ($scope._data===null){
-        $http({method: 'GET', url: "./php/getData.php"}).then(function(_data) {
+        do{
+          //wait
+        }while($scope.myLocation===null)
+        var url = "./php/getData.php?lat=";
+        url += $scope.myLocation.coords.latitude + '&long=' + $scope.myLocation.coords.longitude;
+
+        $http.get(url).then(
+          function(response){
+            this.parseEvent(response)
+          }, function(err) {
+              $('.alert').html('Bad / No data received from server. Check your connection or EventFinda may be down. Results shown are ad-hoc offline activity suggestions. Re-toggle planned activities to try again.');
+              $('.alert').show('0.3s')
+              setTimeout(function(){
+                $('.alert').hide('0.3s')
+              }, 5000)
+              $('.loader').hide();
+              $scope.findAdHocActivity();
+              $scope.onlineStatus = false;
+          })
+
+        parseEvent = function(_data) {
           $scope._data = _data;
           var events = _data.data.events;
           activity = events[Math.floor(Math.random() * events.length)];
           $scope.activity = new $scope.newActivity(activity);
-          console.log(activity.name)
           $scope.result = activity.name + ' at ' + activity.location_summary + " starting at " + activity.datetime_start;
+          $scope.initMap(activity.point.lat, activity.point.lng)
           $('.loader').hide();
-        });
-      } else{
+        }
+      } else {
         console.log('yay! didnt touch')
         var events = $scope._data.data.events;
         activity = events[Math.floor(Math.random() * events.length)];
         console.log(activity.name)
         $scope.activity = new $scope.newActivity(activity);
         $scope.result = activity.name + ' at ' + activity.location_summary + " starting at " + activity.datetime_start;
+        $scope.initMap(activity.point.lat, activity.point.lng)
         $('.loader').hide();
       }
     }
 
     $scope.displayActivity = function(){
-      if($scope.eventCheckBox){
+      if($scope.eventCheckBox && $scope.onlineStatus){
         var random_boolean = Math.random() >= 0.5;
         if(random_boolean){
           $scope.findEventActivity();
@@ -80,5 +106,205 @@ app.controller('mController', function($scope, $interval, $http) {
         $scope.findAdHocActivity();
       }
     }
+
+    $scope.getLocation = function() {
+      if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(pos){
+            console.log(pos)
+            $scope.myLocation = pos;
+          });
+      } else {
+          //x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+
+    $scope.initMap = function(_lat = -36.848448, _lng = 174.762191){
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: _lat, lng: _lng},
+        zoom: 15,
+        styles: [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "stylers": [
+      {
+        "color": "#e2e7d6"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural.terrain",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "visibility": "on"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#d3d7e2"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c3c2ce"
+      },
+      {
+        "weight": 1
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#aec5c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]
+      });
+      var marker = new google.maps.Marker({position: {lat: _lat, lng: _lng}, map: map});
+    }
+
 
 });
--- php/getData.php
+++ php/getData.php
@@ -1,13 +1,18 @@
 <?php
-
   // Request the response in JSON format using the .json extension
-  $url = 'http://api.eventfinda.co.nz/v2/events.json?rows=20&location_slug=auckland&start_date=2018-11-19&fields=event:(id,name,location_summary,datetime_start)';
+
+  $url = 'http://api.eventfinda.co.nz/v2/events.json?rows=50&point=' . $_GET['lat'] . ',' . $_GET['long'] . '&order=distance_date&start_date=2018-11-19&fields=event:(id,name,location_summary,datetime_start,point)';
   $username = 'notnamed';$password = '4skhsb56rsvd';
   $process = curl_init($url);
   curl_setopt($process, CURLOPT_USERPWD, $username . ":" . $password);
   curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
   $return = curl_exec($process);
   header('Content-Type: application/json');
-  echo $return;
+  if(is_null($return['name'])){
+      echo 'error';
+  } else {
+      echo $return;
+  }
+
   $collection = json_decode($return);
   ?>
